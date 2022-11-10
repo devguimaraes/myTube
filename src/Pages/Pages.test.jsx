5a961c7c52effcd3d44c40658598f3a4435fcac0
Bruno Guimaraes <devgmrs@gmail.com>
2022-11-09 22:33:07 -0300

    :test_tube:	test: testes de snapshot

--- src/Pages/Pages.test.jsx
+++ src/Pages/Pages.test.jsx
@@ -1,8 +1,20 @@
+/* eslint-disable arrow-body-style */
 /* eslint-disable no-undef */
 import { render } from '@testing-library/react';
 import { Home } from './Home';
+import { useFiltroContext } from '../Context/Filtro';
+
+jest.mock('../Context/Filtro', () => {
+	return {
+		useFiltroContext: jest.fn(),
+	};
+});
 
 describe('Teste de Snapshot', () => {
+	beforeEach(() => {
+		useFiltroContext.mockReturnValue([]);
+	});
+
 	test('Snpashot da página Home', () => {
 		const { container } = render(<Home />);
 
