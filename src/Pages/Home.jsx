4576ee2ab5345dcf50f096aa379e2d919442c23d
Bruno Guimaraes <devgmrs@gmail.com>
2022-11-11 03:58:31 -0300

    :recycle:	refactor: Refatorcao de código

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,13 +2,12 @@ import { Header } from '../Components/Header/Header';
 import Menu from '../Components/Menu/Index';
 import { Timeline } from '../Components/Timeline/TimeLine';
 import config from '../../config.json';
-import { CSSReset } from '../global';
+
 import { Favorites } from '../Components/Favorites/Favorites';
 
 export function Home() {
 	return (
 		<>
-			<CSSReset />
 			<Menu />
 			<Header />
 			<Timeline playlists={config.playlists} />
